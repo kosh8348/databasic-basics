988baf99f5bc077149a746ca8b0e330c7ffadb51
koseunghyun <kosh8348@gmail.com>
2023-12-19 21:05:17 +0900

    init

---
+++ 03_join/join.sql
@@ -0,0 +1,8 @@
+select * from lecture;
+select * from instructor;
+
+SELECT *
+FROM lecture, instructor;
+-- lecture가 가진 instructor_id
+-- instructor의 id가 일치한다.
+WHERE lecture.instructor_id = instructor.id;
